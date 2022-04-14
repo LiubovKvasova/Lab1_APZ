c41ef4dea4b9a11248db8ebd64747e5087a9217a
Liubov Kvasova <bubochka102003@gmail.com>
2022-04-14 21:27:01 +0300

    Added value timeBroadcast

--- server.go
+++ server.go
@@ -7,7 +7,11 @@ import (
 	"time"
 )
 
-func timeRequest(rw http.ResponseWriter, req *http.Request){}
+func timeRequest(rw http.ResponseWriter, req *http.Request){
+
+	rw.Header().Set("Content-Type", "application/json")
+	timeBroadcast := time.Now().Format(time.RFC3339)
+}
 
 func main() {
   http.ListenAndServe(":8795", nil)
